539381cb58f113c80adc345822bcd583623b8fe1
Hasnat Jamil <hasnatjamil2018@gmail.com>
2020-12-07 23:59:42 +0600

    changeset: index.css: setup base variables, insert css for navbar component app.js: setup routing for navbar Navbar.js: complete the navbar component

--- src/App.js
+++ src/App.js
@@ -1,14 +1,26 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Navbar from './components/Navbar';
 import People from './components/People';
 import Planets from './components/Planets';
 
 const App = () => {
+  const [page, setPage] = useState('planets');
+  const handlePageClick = (to) => {
+    switch (to) {
+      case 'planets':
+        setPage('planets');
+        break;
+      case 'people':
+        setPage('people');
+        break;
+      default:
+        setPage('planets');
+    }
+  };
   return (
     <>
-      <Navbar />
-      <People />
-      <Planets />
+      <Navbar clickHandler={(to) => handlePageClick(to)} currentPage={page} />
+      {page === 'planets' ? <Planets /> : <People />}
     </>
   );
 };
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,13 +1,46 @@
 import React from 'react';
 
-const Navbar = () => {
+const Navbar = ({ clickHandler, currentPage }) => {
   return (
-    <nav>
-      <h3>SWiki</h3>
-      <ul>
-        <li>People</li>
-        <li>Planets</li>
-      </ul>
+    <nav className="nav">
+      <div className="container">
+        <div className="nav__navbar">
+          <h3 className="nav__navbar__title">SWiki</h3>
+          <ul className="nav__navbar__navLink">
+            {currentPage === 'people' ? (
+              <>
+                <li
+                  className="nav__navbar__navLink__link nav__navbar__navLink__link--active"
+                  onClick={() => clickHandler('people')}
+                >
+                  People
+                </li>
+                <li
+                  className="nav__navbar__navLink__link"
+                  onClick={() => clickHandler('planets')}
+                >
+                  Planets
+                </li>
+              </>
+            ) : (
+              <>
+                <li
+                  className="nav__navbar__navLink__link "
+                  onClick={() => clickHandler('people')}
+                >
+                  People
+                </li>
+                <li
+                  className="nav__navbar__navLink__link nav__navbar__navLink__link--active"
+                  onClick={() => clickHandler('planets')}
+                >
+                  Planets
+                </li>
+              </>
+            )}
+          </ul>
+        </div>
+      </div>
     </nav>
   );
 };
