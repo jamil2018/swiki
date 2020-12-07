5fa393a2e8287c32545de03f0278e0e59a7026fd
Hasnat Jamil <hasnatjamil2018@gmail.com>
2020-12-07 23:02:08 +0600

    boilerplate setup

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Navbar from './components/Navbar';
+import People from './components/People';
+import Planets from './components/Planets';
+
+const App = () => {
+  return (
+    <>
+      <Navbar />
+      <People />
+      <Planets />
+    </>
+  );
+};
+
+export default App;
---
+++ src/components/Navbar.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Navbar = () => {
+  return (
+    <nav>
+      <h3>SWiki</h3>
+      <ul>
+        <li>People</li>
+        <li>Planets</li>
+      </ul>
+    </nav>
+  );
+};
+
+export default Navbar;
